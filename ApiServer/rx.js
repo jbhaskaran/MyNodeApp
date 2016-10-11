bcd2dc6f7fea7c14e90f1b379d792b25234790a0
jbhaskaran <jinesh.bhaskaran@gmail.com>
2016-10-10 22:18:45 -0700

    Changes

--- ApiServer/rx.js
+++ ApiServer/rx.js
@@ -4,8 +4,13 @@ var inherits = require('./inherits')
     var noop = function() { },
         defaultError = function(err) { throw err; };
 
+    function isFunction(value) {
+        return typeof value === 'function' || false;
+        //Object.prototype.toString.call(value) === '[object Function]';
+    }
+
     var Observer = function () { };
-    Observer.create = function(onNext, onError, onCompleted) {
+    var observerCreate = Observer.create = function(onNext, onError, onCompleted) {
         onNext = onNext || (onNext = noop);
         onError = onError || (onError = defaultError);
         onCompleted = onCompleted || (onCompleted = noop);
@@ -36,8 +41,25 @@ var inherits = require('./inherits')
         function Observable() {
 
         }
+        Observable.isObservable = function(o) {
+            return o && isFunction(o.subscribe);
+        }
+        Observable.prototype.subscribe = function(onNext, onError, onCompleted) {
+            return observerCreate(onNext, onError, onCompleted);
+        }
         return Observable;
     }());
+
+    var FromObservable = (function(__super__) {
+        inherits(FromObservable, __super__);
+        function FromObservable(iterable, fn, scheduler) {
+            this._iterable = iterable;
+            this._fn = fn;
+            this._scheduler = scheduler;
+            __super__.call(this);
+        }
+        
+    })();
 
 
 
